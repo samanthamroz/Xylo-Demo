59a487c9b9102291bab85564672e14daf86b05ff
Samantha Mroz <mrozsamantha@gmail.com>
2025-03-16 17:11:28 -0400

    Fixed intro tutorial geometry

--- Xylo/Assets/Scripts/MonoBehaviors/Straightener.cs
+++ Xylo/Assets/Scripts/MonoBehaviors/Straightener.cs
@@ -7,8 +7,8 @@ public class Straightener : MonoBehaviour
     {
         if (other.gameObject.TryGetComponent<Rigidbody>(out var rb)) {
             //Debug.Log("velocity at enter: " + rb.velocity);
-            Vector3 newVelocity = new Vector3(rb.velocity.x * straightenTo.x, rb.velocity.y * straightenTo.y, rb.velocity.z * straightenTo.z).normalized;
-            newVelocity *= rb.velocity.magnitude;
+            Vector3 maskedVelocity = new Vector3(rb.velocity.x * straightenTo.x, rb.velocity.y * straightenTo.y, rb.velocity.z * straightenTo.z);
+            Vector3 newVelocity = maskedVelocity.normalized * rb.velocity.magnitude;
             rb.velocity = newVelocity;
             //Debug.Log("new velocity: " + rb.velocity);
         }
