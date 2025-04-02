fe9e72b877c33c6aea647694fd2afb3773db5fe6
Samantha Mroz <mrozsamantha@gmail.com>
2025-04-02 13:35:30 -0400

    Migrated block movement to new input system

--- Xylo/Assets/Scripts/MonoBehaviors/DraggableBlock.cs
+++ Xylo/Assets/Scripts/MonoBehaviors/DraggableBlock.cs
@@ -1,57 +1,25 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class DraggableBlock : MonoBehaviour
 {
     public Note note;
-    private Vector3 mousePosition;
+    private Vector3 mousePosition { get { return MouseManager.self.mousePosition; } }
     private Vector3 originalPos;
     private Vector3 direction = Vector3.one;
+    public bool isDragging;
     void Start()
     {
+        isDragging = false;
         originalPos = GetRoundedVector(transform.localPosition);
     }
     private Vector3 GetRoundedVector(Vector3 vec) {
         return new Vector3((float)Math.Round(vec.x), (float)(Math.Round(vec.y * 2)/2), (float)Math.Round(vec.z));
     }
-    private Vector3 GetMousePosition() {
-        return Camera.main.WorldToScreenPoint(transform.position);
-    }
-    void OnMouseClick() {
-
-    }
-    private void OnMouseDown() {
-        originalPos = GetRoundedVector(transform.localPosition);
-        mousePosition = Input.mousePosition - GetMousePosition();
+    public void OnMouseDown() {
         GetComponent<AudioSource>().Play();
-    }
-    private void OnMouseDrag()
-    {
-        Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
-        newPos = GetRoundedVector(newPos);
-
-        if (direction == Vector3.zero) {
-            direction = Vector3.one;
-        }
-
-        if (direction.x == 0) {
-            newPos.x = originalPos.x;
-        }
-        if (direction.y == 0) {
-            newPos.y = originalPos.y;
-        }
-        if (direction.z == 0) {
-            newPos.z = originalPos.z;
-        }
-
-        if (direction == Vector3.one) {
-            direction = newPos - originalPos;
-        }
-        if (!IsCollidingAtPosition(newPos) && IsValidMovement(newPos))
-        {
-            transform.localPosition = newPos;
-        }
     }
     private bool IsCollidingAtPosition(Vector3 position)
     {
@@ -74,10 +42,45 @@ public class DraggableBlock : MonoBehaviour
             Math.Abs(transform.position.y - position.y) <= .5 &&
             Math.Abs(transform.position.z - position.z) <= 1;
     }
-    void OnMouseUp()
-    {
+
+    public IEnumerator Drag() {
+        isDragging = true;
+        Vector3 originalPosition = GetRoundedVector(transform.localPosition);
+        Vector3 newWorldPosition;
+        while(isDragging) {
+            float z = Camera.main.WorldToScreenPoint(transform.position).z;
+            newWorldPosition = GetRoundedVector(Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, z)));
+            print(newWorldPosition);
+            
+            //if we haven't moved yet, let it move in any direction
+            if (direction == Vector3.zero) {
+                direction = Vector3.one;
+                direction = newWorldPosition - originalPos; //locks direction
+            } else {
+                //we must be currently moving
+                if (direction.x == 0) {
+                    newWorldPosition.x = originalPos.x;
+                }
+                if (direction.y == 0) {
+                    newWorldPosition.y = originalPos.y;
+                }
+                if (direction.z == 0) {
+                    newWorldPosition.z = originalPos.z;
+                }
+            }
+
+            //check for collisions, etc, then move
+            if (!IsCollidingAtPosition(newWorldPosition) && IsValidMovement(newWorldPosition))
+            {
+                transform.localPosition = newWorldPosition;
+            }
+
+            yield return null;
+        }
+        //drop
         direction = Vector3.one;
     }
+
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Marble")) {
--- Xylo/Assets/Scripts/MonoBehaviors/Singletons/MouseManager.cs
+++ Xylo/Assets/Scripts/MonoBehaviors/Singletons/MouseManager.cs
@@ -1,11 +1,30 @@
+using System.Linq.Expressions;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
+//source: https://www.youtube.com/watch?v=zo1dkYfIJVg
 
 public class MouseManager : MonoBehaviour
 {
     public static MouseManager self;
     public Vector3 mousePosition;
-
+    public DraggableBlock currentInteractable;
+    private bool isInteractable {
+        get {
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                try
+                {
+                    currentInteractable = hit.collider.gameObject.GetComponent<DraggableBlock>();
+                    return true;
+                } finally {}
+            }
+            return false;
+        }
+    }
+    
     void Awake() {
 		if (self == null) {
 			self = this;
@@ -14,8 +33,19 @@ public class MouseManager : MonoBehaviour
 			Destroy(gameObject);
 		}
     }
-
+    
     void OnMouseMove(InputValue value) {
         mousePosition = value.Get<Vector2>();
+    }
+    
+    void OnMouseClick(InputValue value) {
+        if (value.Get<float>() == 1) { //click down
+			if (isInteractable) {
+                StartCoroutine(currentInteractable.Drag());
+            }
+		} else { //click released
+            currentInteractable.isDragging = false;
+            currentInteractable = null;
+        }
     }
 }
