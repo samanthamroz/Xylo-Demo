2d70347a9a38282cda35b81d63972fa1c32936b6
Samantha Mroz <mrozsamantha@gmail.com>
2025-04-28 15:35:03 -0400

    fixed pause menu

---
+++ Xylo/Assets/Scripts/MonoBehaviors/UI Objects/QuitButton.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    public void quitGame() {
+        Application.Quit(0);
+    }
+}
